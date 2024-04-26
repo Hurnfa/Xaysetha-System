a58cc129ad3eb5fd4c915387e13f69d8948716b1
Hurnfa <99794184+Hurnfa@users.noreply.github.com>
2024-04-26 16:05:34 +0700

    Change Label to Original

--- Xaysetha System/Form1.Designer.cs
+++ Xaysetha System/Form1.Designer.cs
@@ -47,10 +47,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Noto Sans Lao", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(145, 104);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(193, 128);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(200, 53);
+            this.label1.Size = new System.Drawing.Size(250, 65);
             this.label1.TabIndex = 1;
             this.label1.Text = "ລົງທະບຽນຜູ້ໃຊ້";
             // 
@@ -59,10 +58,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Noto Sans Lao", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(95)))), ((int)(((byte)(113)))));
-            this.label2.Location = new System.Drawing.Point(92, 241);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(123, 297);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 31);
+            this.label2.Size = new System.Drawing.Size(110, 38);
             this.label2.TabIndex = 3;
             this.label2.Text = "ຊື່ບັນຊີຜູ້ໃຊ້";
             // 
@@ -71,10 +69,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Noto Sans Lao", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(95)))), ((int)(((byte)(113)))));
-            this.label3.Location = new System.Drawing.Point(92, 345);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(123, 425);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(91, 31);
+            this.label3.Size = new System.Drawing.Size(110, 38);
             this.label3.TabIndex = 5;
             this.label3.Text = "ລະຫັດຜ່ານ";
             // 
@@ -82,11 +79,10 @@
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Noto Sans Lao", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.LinkColor = System.Drawing.Color.Green;
-            this.linkLabel1.Location = new System.Drawing.Point(368, 432);
-            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(149)))), ((int)(((byte)(255)))));
+            this.linkLabel1.Location = new System.Drawing.Point(491, 532);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(108, 26);
+            this.linkLabel1.Size = new System.Drawing.Size(135, 33);
             this.linkLabel1.TabIndex = 6;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "ລືມລະຫັດຜ່ານ ?";
@@ -101,10 +97,10 @@
             this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(201)))));
             this.guna2Button1.Font = new System.Drawing.Font("Noto Sans Lao", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button1.ForeColor = System.Drawing.Color.White;
-            this.guna2Button1.Location = new System.Drawing.Point(92, 608);
-            this.guna2Button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.guna2Button1.Location = new System.Drawing.Point(123, 748);
+            this.guna2Button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(379, 46);
+            this.guna2Button1.Size = new System.Drawing.Size(505, 57);
             this.guna2Button1.TabIndex = 7;
             this.guna2Button1.Text = "ເຂົ້າສູ່ລະບົບ";
             // 
@@ -113,10 +109,9 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Noto Sans Lao", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(95)))), ((int)(((byte)(113)))));
-            this.label4.Location = new System.Drawing.Point(184, 660);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(245, 812);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(104, 25);
+            this.label4.Size = new System.Drawing.Size(124, 29);
             this.label4.TabIndex = 8;
             this.label4.Text = "ທ່ານຍັງບໍ່ມີບັນຊີ?";
             // 
@@ -125,10 +120,9 @@
             this.linkLabel2.AutoSize = true;
             this.linkLabel2.Font = new System.Drawing.Font("Noto Sans Lao", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabel2.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(149)))), ((int)(((byte)(255)))));
-            this.linkLabel2.Location = new System.Drawing.Point(272, 660);
-            this.linkLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel2.Location = new System.Drawing.Point(363, 812);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(71, 25);
+            this.linkLabel2.Size = new System.Drawing.Size(83, 29);
             this.linkLabel2.TabIndex = 9;
             this.linkLabel2.TabStop = true;
             this.linkLabel2.Text = "ລົງທະບຽນ";
@@ -138,10 +132,10 @@
             this.guna2PictureBox1.BorderRadius = 15;
             this.guna2PictureBox1.Image = global::Xaysetha_System.Properties.Resources.Group_20479219;
             this.guna2PictureBox1.ImageRotate = 0F;
-            this.guna2PictureBox1.Location = new System.Drawing.Point(520, 20);
-            this.guna2PictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.guna2PictureBox1.Location = new System.Drawing.Point(693, 25);
+            this.guna2PictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.guna2PictureBox1.Name = "guna2PictureBox1";
-            this.guna2PictureBox1.Size = new System.Drawing.Size(541, 699);
+            this.guna2PictureBox1.Size = new System.Drawing.Size(721, 860);
             this.guna2PictureBox1.TabIndex = 11;
             this.guna2PictureBox1.TabStop = false;
             // 
@@ -163,14 +157,14 @@
             this.guna2TextBox2.IconLeft = global::Xaysetha_System.Properties.Resources.Group_102;
             this.guna2TextBox2.IconLeftOffset = new System.Drawing.Point(12, 0);
             this.guna2TextBox2.IconLeftSize = new System.Drawing.Size(24, 24);
-            this.guna2TextBox2.Location = new System.Drawing.Point(92, 384);
-            this.guna2TextBox2.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.guna2TextBox2.Location = new System.Drawing.Point(123, 473);
+            this.guna2TextBox2.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
             this.guna2TextBox2.Name = "guna2TextBox2";
             this.guna2TextBox2.PasswordChar = '\0';
             this.guna2TextBox2.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
             this.guna2TextBox2.PlaceholderText = "ປ້ອນຊື່ບັນຊີຜູ້ໃຊ້..";
             this.guna2TextBox2.SelectedText = "";
-            this.guna2TextBox2.Size = new System.Drawing.Size(379, 46);
+            this.guna2TextBox2.Size = new System.Drawing.Size(505, 57);
             this.guna2TextBox2.TabIndex = 4;
             // 
             // guna2TextBox1
@@ -191,33 +185,33 @@
             this.guna2TextBox1.IconLeft = global::Xaysetha_System.Properties.Resources.Person;
             this.guna2TextBox1.IconLeftOffset = new System.Drawing.Point(12, 0);
             this.guna2TextBox1.IconLeftSize = new System.Drawing.Size(24, 24);
-            this.guna2TextBox1.Location = new System.Drawing.Point(92, 280);
-            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.guna2TextBox1.Location = new System.Drawing.Point(123, 345);
+            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
             this.guna2TextBox1.Name = "guna2TextBox1";
             this.guna2TextBox1.PasswordChar = '\0';
             this.guna2TextBox1.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
             this.guna2TextBox1.PlaceholderText = "ປ້ອນຊື່ບັນຊີຜູ້ໃຊ້..";
             this.guna2TextBox1.SelectedText = "";
-            this.guna2TextBox1.Size = new System.Drawing.Size(379, 46);
+            this.guna2TextBox1.Size = new System.Drawing.Size(505, 57);
             this.guna2TextBox1.TabIndex = 2;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Xaysetha_System.Properties.Resources.image_removebg_preview__3_;
-            this.pictureBox1.Location = new System.Drawing.Point(18, 20);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(24, 25);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(60, 65);
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1080, 734);
+            this.ClientSize = new System.Drawing.Size(1440, 903);
             this.Controls.Add(this.guna2PictureBox1);
             this.Controls.Add(this.linkLabel2);
             this.Controls.Add(this.label4);
@@ -230,7 +224,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
