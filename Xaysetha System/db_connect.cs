6d9b294b8c3b397e56166a5331bf783413c9b99d
Bozzadyy <bigbosssecon5@gmail.com>
2024-06-30 12:15:47 +0700

    change connection to PostgreSQL

--- Xaysetha System/db_connect.cs
+++ Xaysetha System/db_connect.cs
@@ -6,26 +6,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace Xaysetha_System
 {
     public class db_connect
     {
-        public MySqlConnection conn = new MySqlConnection();
+        //public MySqlConnection conn = new MySqlConnection();
+        public NpgsqlConnection conn = new NpgsqlConnection();
         //public string path = "server=localhost; username=root; database=db_picture; port=3306; password=";
+        public string path = "User Id=postgres.vikiezgjlynnublheair;Password=[YOUR-PASSWORD];Server=aws-0-ap-southeast-1.pooler.supabase.com;Port=6543;Database=postgres;";
         //string pwd = "Risd8446+-";
 
         public string userName { get; set; }
         public string password { get; set; }
-        private string path;
+        //private string path;
 
 
-        public string GetConnectionString()
+/*        public string GetConnectionString()
         {
             string pathString = "server=35.220.141.128; username=" + userName + "; database=simple_db; port=3306; password=" + password +"";
 
             return path = pathString;
-        }
+        }*/
 
         public void getConnect()
         {
@@ -39,6 +42,7 @@ namespace Xaysetha_System
                 {
                     conn.ConnectionString = path;
                     conn.Open();
+                    MessageBox.Show("Connected!");
                 }
                 catch (Exception ex)
                 {
